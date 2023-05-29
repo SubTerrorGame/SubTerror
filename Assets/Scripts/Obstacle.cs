b5852b3b7fe7d9fd1847229709e1cd4c695d3595
PrasinShrestha <prasinloneranger124@gmail.com>
2023-05-29 16:07:58 -0400

    integrated mvp commit

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float leftEdge;
+
+    private void Start()
+    {
+        //leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 2f;
+        leftEdge = -13.48f;
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.left * GameManager.instance.gameSpeed * Time.deltaTime;
+        if (transform.position.x < leftEdge)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/UI/Terra_PlayerCntroller.cs
+++ Assets/Scripts/UI/Terra_PlayerCntroller.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class Terra_PlayerCntroller : MonoBehaviour
 {
     public float jumpTime, jumpHeight, slideTime;
-
-    BoxCollider2D terraCollider;
+    private float jumpTimeInRad, jumpTimeElapsed;
+   // BoxCollider terraCollider;
     //Box Collider size in different for different sprints. All are (0,0) offset
-    private Vector2 colliderRunSize, colliderSlideSize, colliderJumpSize;
+   // private Vector2 colliderRunSize, colliderSlideSize, colliderJumpSize;
 
     Animator animator;
-    private bool isInAction = false; //Action is state where Terra is jumping or sliding. Anything any situation when Terra should ignore player control
-
+    private bool isInAction = false, jumping = false; //Action is state where Terra is jumping or sliding. Anything any situation when Terra should ignore player control
+    private Vector3 defaultPosition;
     // Start is called before the first frame update
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
         //Box Collider size in different for different sprints. All are (0,0) offset
-        colliderRunSize = new Vector2(0.68f, 1.04f); 
-        colliderSlideSize = new Vector2(1.16f, 0.68f);
-        colliderJumpSize = new Vector2(0.57f, 1.04f);
+       // colliderRunSize = new Vector3(0.68f, 1.040f, 0.2f); 
+       // colliderSlideSize = new Vector3(1.16f, 0.68f, 0.2f);
+       // colliderJumpSize = new Vector3(0.57f, 1.04f, 0.2f);
         //get collider and set size
-        terraCollider = gameObject.GetComponent<BoxCollider2D>();
-        terraCollider.size = colliderRunSize;
+       // terraCollider = gameObject.GetComponent<BoxCollider>();
+       // terraCollider.size = colliderRunSize;
+        defaultPosition = transform.localPosition;
+        jumpTimeInRad = Mathf.PI/jumpTime;
     }
 
     // Update is called once per frame
@@ -31,18 +33,28 @@ public class Terra_PlayerCntroller : MonoBehaviour
     {
         if(isInAction)
         {
+            if(jumping)
+            {
+                //some math with sin to get the jumping arc
+                float normalizedJumpTimeToPI = (jumpTimeElapsed) * jumpTimeInRad;
+                jumpTimeElapsed += Time.deltaTime;
+                float jumpingHeight = jumpHeight * Mathf.Sin(normalizedJumpTimeToPI);
+                //apply to position
+                Vector3 newLocation =  new Vector3(defaultPosition.x, defaultPosition.y + jumpingHeight, defaultPosition.z);
+                transform.localPosition = newLocation;
+            }
             return;
         }
         //Jump
-        if(Input.GetAxis("Vertical") > 0)
+        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) )
         {
+            jumpTimeElapsed = 0; //reset Jumptime elapsed from past jump
             StartCoroutine(terraJump(jumpTime));
         }
         //Slide
-        if(Input.GetAxis("Vertical") < 0)
+        if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) )
         {
             StartCoroutine(terraSlide(slideTime));
-
         }
     }
 
@@ -50,15 +62,16 @@ public class Terra_PlayerCntroller : MonoBehaviour
     {
 
         isInAction = true;
+        jumping = true;
         animator.SetBool("Jump", true);
-        terraCollider.size = colliderJumpSize;
+     //   terraCollider.size = colliderJumpSize;
 
-        //some math with sin to getthe jumping arc
 
         yield return new WaitForSeconds(jumpTime);
 
         isInAction = false;
-        terraCollider.size = colliderRunSize;
+        jumping = false;
+      //  terraCollider.size = colliderRunSize;
         animator.SetBool("Jump", false);
 
     }
@@ -66,12 +79,23 @@ public class Terra_PlayerCntroller : MonoBehaviour
     {
         isInAction = true;
         animator.SetBool("Slide", true);
-        terraCollider.size = colliderSlideSize;
+       // terraCollider.size = colliderSlideSize;
 
         yield return new WaitForSeconds(slideTime);
 
         isInAction = false;
-        terraCollider.size = colliderRunSize;
+      //  terraCollider.size = colliderRunSize;
         animator.SetBool("Slide", false);
     }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            Debug.Log("Collide");
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+
 }
